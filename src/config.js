b005a26fd7ecdc13d4110a5093e5b9370d8f6f63
Abdykali Azat <azat.abdykalli@gmail.com>
2024-11-24 15:45:07 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://gold-cargo.kz:3001',
+   
+    // nameCargo: 'TURKESTAN KARGO'
+    nameCargo: 'Gold Cargo'
+  };
+  
+  
+export default config;
+  
